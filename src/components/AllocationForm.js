07d77bcd9b99f292d26ed3fe472a27cae88cc488
Jesus Salvacion <jsswp2004@outlook>
2024-01-01 14:26:05 -0500

    cc

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -60,15 +60,15 @@ const AllocationForm = (props) => {
                         <option value="HR" name="hr">HR</option>
                         <option value="IT" name="it">IT</option>
                         <option value="Admin" name="admin">Admin</option>
-                  </select>
+                    </select>
 
                     <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
-                <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
-                  </div>
-                  <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
+                        <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
+                    </div>
+                    <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
                         <option defaultValue value="Add" name="Add">Add</option>
-                <option value="Reduce" name="Reduce">Reduce</option>
-                  </select>
+                        <option value="Reduce" name="Reduce">Reduce</option>
+                    </select>
 
                     <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
                         <label className="input-group-text" htmlFor="cost">£{currency}</label> {/* Currency Prefix */}
@@ -85,7 +85,7 @@ const AllocationForm = (props) => {
                         </input>
                     </div>
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
-                        Save
+                        Saves
                     </button>
                 </div>
             </div>
